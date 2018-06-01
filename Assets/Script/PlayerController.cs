6d6d9fc2f4f83f524bfe83c16601ec2b790ad5a9
johannesridho <johanridho@gmail.com>
2018-06-01 21:55:49 +0700

    add all assets for demo

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private int SPEED = 20;
+	
+	void Start () {
+		
+	}
+	
+    void Update () {
+        Vector2 touchAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad) * Time.deltaTime * SPEED;
+        transform.position += new Vector3(touchAxis.x, 0, touchAxis.y);
+	}
+}
